307995d0f49d14eed86b6748cda3357bea6203b2
hyeong3642@kookmin.ac.kr <hyeong3642@kookmin.ac.kr>
2017-09-08 08:13:47 +0900

    trying to correct exception handling

--- week2/20171672-이원형-assignment2.py
+++ week2/20171672-이원형-assignment2.py
@@ -4,9 +4,13 @@ def factorial(n):
 	else:
 		return n * factorial(n - 1)
 while True:
-	a = int(input("input number : "))
+	a = input("input number : ")
 	if a == -1:
 		break
+	elif int(a) != float(a):
+		print ("please int integer")
+		a = int(input("input number again :"))
+
 	else:
 		print(factorial(a))
 	
