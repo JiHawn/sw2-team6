d979e28c64d57311aa235ee913389c535f2ebeef
hyeong3642@kookmin.ac.kr <hyeong3642@kookmin.ac.kr>
2017-09-08 08:04:42 +0900

    correct as coding rule

--- week2/20171672-이원형-assignment2.py
+++ week2/20171672-이원형-assignment2.py
@@ -1,11 +1,11 @@
-def factorial(n) :
-	if n == 0 :
+def factorial(n):
+	if n == 0:
 		return 1
-	else :
-		return n * factorial (n-1)
-while True :
+	else:
+		return n * factorial(n - 1)
+while True:
 	a = int(input("input number : "))
-	if a == -1 :
+	if a == -1:
 		break
 	else:
 		print(factorial(a))
