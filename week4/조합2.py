b35af93a263b34ce1e34358943390e9d4ace2bd8
Eunji Lee <ji0859@kookmin.ac.kr>
2017-09-28 06:54:23 +0900

    factorial을 이용하지 않음

---
+++ week4/조합2.py
@@ -0,0 +1,19 @@
+#팩토리얼을 이용하지 않고 구하기
+def fact(n, m):
+    sum = 1
+    while n >= m:
+        sum *=n
+        n -=1
+        return n
+    return sum
+
+def fac(j):
+    um = 1
+    for k in range(j +1):
+        k +=1
+        um *=k
+    return um
+a = int(input("nCr 의 n을 입력하시오."))
+b = int(input("nCr 의 r을 입력하시오."))
+c  = print (fact(a, b))
+print (c/fac(b))
