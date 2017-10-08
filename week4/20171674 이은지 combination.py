d35472f9d09b02b8516785e432b2c660d6c8feb0
ji0859 <ji0859@kookmin.ac.kr>
2017-10-08 15:14:34 +0900

    Update and rename combination.py to 20171674 이은지 combination.py

--- week4/20171674 이은지 combination.py
+++ week4/20171674 이은지 combination.py
@@ -1,21 +1,22 @@
 # 팀별 수업 이후 
 #효율성 검사를 시간을 재서 factorial을 이용한 코드와 안이용한 코드 시간비교 
 import time 
-def fact(n) :
-      if n == 0 :
+#factorial 이용
+def fact(n):
+      if n == 0:
             return 1
-      else :
+      else:
             return n * fact(n - 1)
 
-def combi(n, r) :
+def combi(n, r):
       return fact(n) / (fact(r) * fact(n-r))
-
-def combi2(n,r) :
-      if r == 0 :
+# factorial 이용안함
+def combi2(n,r):
+      if r == 0:
             return 1
-      elif n < r :
+      elif n < r:
             return 0
-      else :
+      else:
             return combi(n-1, r-1) + combi(n-1, r)
 while True:
 	n = int(input("첫번째 수를 입력 또는 -1을 입력하면 종료:"))
