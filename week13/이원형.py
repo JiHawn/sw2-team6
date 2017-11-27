af85a0ffda086f3edaaf251983f8429a10b29c22
hyeong3642@kookmin.ac.kr <hyeong3642@kookmin.ac.kr>
2017-11-28 08:42:13 +0900

    이원형

---
+++ week13/이원형.py
@@ -0,0 +1,34 @@
+class Guess:
+
+    def __init__(self, word):
+
+        self.secretWord = word
+        self.guessedChars = set()
+        self.numTries = 0
+        self.currentStatus = '_' * len(word)
+
+
+    def display(self):
+        print('Current : ' + self.currentStatus)
+        print('Tries : ' + str(self.numTries))
+
+    def guess(self, character):
+        self.guessedChars = self.guessedChars.union({character})
+        if character not in self.secretWord:
+            self.numTries += 1
+            return  False
+        else:
+            currentStatus = ''
+            for i in self.secretWord:
+                if i in self.guessedChars:
+                    currentStatus += i
+                else:
+                    currentStatus += '_'
+            self.currentStatus = currentStatus
+        checker = None
+        if self.currentStatus == self.secretWord :
+            checker = True
+        else :
+            checker = False
+
+            return checker
